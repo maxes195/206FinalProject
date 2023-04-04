3c3657233d5c59ef070d1d97fe264f2610070b77
Maxes195 <brennan.laing@gmail.com>
2023-04-04 11:58:55 -0600

    Added boilerplate to project

---
+++ Code.s
@@ -0,0 +1,21 @@
+;|Final Project
+;|-----------------------------
+;|Created By:   Brennan Laing
+;|Class:        ITSC 204
+;|Date:         04/04/2023 
+;|      x86-64, NASM
+;|-----------------------------
+
+global _start
+section .text ; Stores instructions for the computer to follow
+
+_start:
+
+
+
+_end:
+    mov rax, 0x3C
+    mov rdi, 0x00
+    syscall
+
+section .data ; Where you declare and store data, static
